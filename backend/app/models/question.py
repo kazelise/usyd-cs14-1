53201ee126ae843f8c0cf480da10f25cc693879e
CroissantSong <164138407+CroissantSong@users.noreply.github.com>
2026-04-19 03:36:36 +1000

    Fix backend lint and formatting

--- backend/app/models/question.py
+++ backend/app/models/question.py
@@ -2,7 +2,7 @@
 
 from datetime import datetime
 
-from sqlalchemy import ForeignKey, JSON, String, Text
+from sqlalchemy import JSON, ForeignKey, String, Text
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.database import Base
